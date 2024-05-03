ac65076da517f0f5532629b644181816a75fb429
Shivanshika <shivanshikaagarwal7275@gmail.com>
2024-05-03 11:05:11 +0530

    main

--- src/components/LearningOptions.jsx
+++ src/components/LearningOptions.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./LearningOptions.css";
 
 const LearningOptions = (props) => {
   const options = [
--- src/components/LinkedList.jsx
+++ src/components/LinkedList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./LinkedList.css";
 
 const LinkList = (props) => {
   console.log(props);
